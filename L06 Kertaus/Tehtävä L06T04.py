35b06c7855039c5288e21fd8306e8405d3e08c74
Arjane JAMK <ad1945@student.jamk.fi>
2022-10-28 18:10:35 +0300

    Updated L06T04

--- L06 Kertaus/Tehtävä L06T04.py
+++ L06 Kertaus/Tehtävä L06T04.py
@@ -2,17 +2,21 @@
 
 # request the points per jump
 def hyppy_pisteet():
-    pisteet_yhteensa = []
+    pisteet_yhteensa = 0
+    isoin = 0
+    pienin = 20
     for x in range(5):
         luku = int(input("Hypyn pisteet: "))
-        pisteet_yhteensa.append(luku)
+        pisteet_yhteensa += luku
 
-# remove the lowest and highest score
-    pisteet_yhteensa.sort()
-    pisteet_yhteensa.pop(0)
-    pisteet_yhteensa.pop(-1)
-    kaiket_pisteet = sum(pisteet_yhteensa)
-    print(f"Pisteet yhteensä: {kaiket_pisteet}")
+        # determine the highest and lowest score, and remove them from the total score
+        if luku > isoin:
+            isoin = luku
+        if luku < pienin:
+            pienin = luku
+    pisteet_yhteensa = pisteet_yhteensa - isoin - pienin
+
+    print(f"Pisteet yhteensä: {pisteet_yhteensa}")
 
 # carry out the program
 hyppy_pisteet()
