100731782e5618918156ffa7004c10bc3c769fbc
Arjane JAMK <ad1945@student.jamk.fi>
2022-10-28 18:30:12 +0300

    Updated L03T02

--- L03 Ehtorakenteet/Tehtävä L03T02.py
+++ L03 Ehtorakenteet/Tehtävä L03T02.py
@@ -5,8 +5,8 @@ luku3 = int(input("Anna kolmas kokonaisluku: "))
 
 # print the biggest given number
 if luku1 > luku2 and luku1 > luku3:
-        print(f"Suurin: {luku1}")
+    print(f"Suurin: {luku1}")
 elif luku2 > luku1 and luku2 > luku3:
-        print(f"Suurin: {luku2}")
+    print(f"Suurin: {luku2}")
 else:
     print(f"Suurin: {luku3}")
